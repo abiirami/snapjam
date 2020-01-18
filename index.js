21d807ed2d5b1bf423beb8e3e89885ea9a27b723
Abi Patchaiyappan <Abi@ucsc-guest-169-233-115-173.ucsc.edu>
2020-01-17 22:52:15 -0800

    hi

---
+++ index.js
@@ -0,0 +1,7 @@
+const request = require('request');
+
+request('https://vision.googleapis.com', { json: true }, (err, res, body) => {
+  if (err) { return console.log(err); }
+  console.log(body.url);
+  console.log(body.explanation);
+});
