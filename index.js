1d3c46ea411f68670debc51fed753e54880d6350
Abi Patchaiyappan <Abi@ucsc-guest-169-233-115-173.ucsc.edu>
2020-01-17 23:13:07 -0800

    server request

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
+
+var http = require('http');
+
+//create a server object:
+http.createServer(function (req, res) {
+  res.write('SNAP JAMS'); //write a response to the client
+  res.end(); //end the response
+}).listen(8080); //the server object listens on port 8080
 const request = require('request');
 
+
+
+// ********** Calling google API *********
 request('https://vision.googleapis.com', { json: true }, (err, res, body) => {
   if (err) { return console.log(err); }
   console.log(body.url);
