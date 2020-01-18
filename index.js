e10cfe93446d2ee440d8d614333a6ce0536596a3
Abi Patchaiyappan <Abi@college10-169-233-115-173.resnet.ucsc.edu>
2020-01-18 00:05:37 -0800

    gitignore added

--- index.js
+++ index.js
@@ -10,9 +10,3 @@ const request = require('request');
 
 
 
-// ********** Calling google API *********
-request('https://vision.googleapis.com', { json: true }, (err, res, body) => {
-  if (err) { return console.log(err); }
-  console.log(body.url);
-  console.log(body.explanation);
-});
