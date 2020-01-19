7f0c03717542256e1907cd2482fadde9c69418e8
Abi <abi2rami@gmail.com>
2020-01-19 04:40:14 -0800

    Delete google.js

--- google.js
+++
@@ -1,7 +0,0 @@
-const request = require('request');
-// ********** Calling google API *********
-request('https://vision.googleapis.com', { json: true }, (err, res, body) => {
-  if (err) { return console.log(err); }
-  console.log(body.url);
-  console.log(body.explanation);
-});
